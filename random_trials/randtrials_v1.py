0b60de07b85868e8db5e669f7260e0218c9c8bb7
natwijaya19 <72418586+natwijaya19@users.noreply.github.com>
2022-07-09 15:23:10 +0700

    kkk

--- random_trials/randtrials_v1.py
+++ random_trials/randtrials_v1.py
@@ -1,28 +1,22 @@
-#%%
-import platform
+# %%
 import sys
+
 print("user current version: ", sys.version)
 
-#%%
+# %%
 from platform import python_version
 
 print("user current version: ", python_version())
 
-
-#%%
+# %%
 
 import pandas as pd
 import numpy as np
+
 from matplotlib import pyplot as plt
 
-rand_data= np.random.randint(low=-5,high=200, size=(10,4))
+rand_data = np.random.randint(low=-5, high=200, size=(10, 4))
 rand_df = pd.DataFrame(rand_data, columns=['col1', 'col2', 'col3', 'col4'])
 print(rand_data)
 print(rand_df)
 print(rand_df)
-
-
-
-
-
-
