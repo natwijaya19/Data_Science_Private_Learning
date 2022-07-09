e2921ea1d5fc287dabd1176a2cfae99adbd4bc72
natwijaya19 <72418586+natwijaya19@users.noreply.github.com>
2022-07-09 17:58:44 +0700

    new file

--- random_trials/learn_python_v1.py
+++ random_trials/learn_python_v1.py
@@ -1,22 +1,27 @@
-#%%
+# %%
+l = [1, 2, 3, 4]
+for item in map(lambda n: n*2, l):
+    print(item)
 
+# %%
+l = [1, 2, 3, 4]
+for item in filter(lambda n: n<4, l):
+    print(item)
 
-#%%
+# %%
+l = [1, 2, 3, 4]
+for item in l:
+    result  = item*2
+    print(result)
 
+s# %%
 
+# %%
 
+# %%
 
-#%%
+# %%
 
+# %%
 
-#%%
-
-#%%
-
-#%%
-
-#%%
-
-#%%
-
-#%%s
+# %%s
