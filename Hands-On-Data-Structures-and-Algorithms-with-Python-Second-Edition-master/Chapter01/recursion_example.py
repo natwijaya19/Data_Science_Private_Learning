9130c7dd45ea7bd495d6b5309faf6c11c4483041
natwijaya19 <72418586+natwijaya19@users.noreply.github.com>
2022-07-09 21:15:11 +0700

    new file

--- Hands-On-Data-Structures-and-Algorithms-with-Python-Second-Edition-master/Chapter01/recursion_example.py
+++ Hands-On-Data-Structures-and-Algorithms-with-Python-Second-Edition-master/Chapter01/recursion_example.py
@@ -1,12 +1,13 @@
 def iterTest(low, high):
-	while low<= high:
-		print(low)
-		low = low+1
+    while low <= high:
+        print(low)
+        low = low + 1
 
-def recurTest (low, high):
-	if low <= high:
-		print(low)
-		recurTest(low+1, high)		
+
+def recurTest(low, high):
+    if low <= high:
+        print(low)
+        recurTest(low + 1, high)
 
 
 print("print values using iteration")
--- random_trials/learn_python_v1.py
+++ random_trials/learn_python_v1.py
@@ -1,20 +1,22 @@
 # %%
 l = [1, 2, 3, 4]
-for item in map(lambda n: n*2, l):
+for item in map(lambda n: n * 2, l):
     print(item)
 
 # %%
 l = [1, 2, 3, 4]
-for item in filter(lambda n: n<4, l):
+for item in filter(lambda n: n < 4, l):
     print(item)
 
 # %%
 l = [1, 2, 3, 4]
 for item in l:
-    result  = item*2
+    result = item * 2
     print(result)
 
-s# %%
+# %%
+
+
 
 # %%
 
